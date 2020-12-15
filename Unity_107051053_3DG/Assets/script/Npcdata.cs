ca7e4821b23a0c08e894b31367b61a8320181924
miyanoillis <71679915+miyanoillis@users.noreply.github.com>
2020-12-15 16:02:07 +0800

    0.3

---
+++ Unity_107051053_3DG/Assets/script/Npcdata.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "npcdata", menuName = "npc/talkdata")]
+public class Npcdata : ScriptableObject
+{
+    [Header("talk1"), TextArea(1, 5)]
+    public string ntalk1;
+    [Header("talk2"), TextArea(1, 5)]
+    public string ntalk2;
+    [Header("talk3"), TextArea(1, 5)]
+    public string ntalk3;
+
+    [Header("need num")]
+    public int num;
+    public int neednum;
+}
---
+++ Unity_107051053_3DG/Assets/script/npc.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class npc : MonoBehaviour
+{
+    [Header("NPCdata")]
+    public Npcdata data;
+    [Header("NPCword")]
+    public GameObject word;
+    public Text text;
+
+    public bool playerin;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "free_male_1")
+        {
+            playerin = true;
+            word.SetActive(true);
+            talk();
+        }
+        else
+        {
+            playerin = false;
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "free_male_1")
+        {
+            playerin = false;
+        }
+    }
+
+    private void talk ()
+    {
+        for (int i = 0; i < data.ntalk1.Length; i++)
+        {
+            print(data.ntalk1[i]);
+        }       
+    }
+}
