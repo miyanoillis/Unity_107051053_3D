f5eaeee7905a43078ebb2f111370f286574afb54
miyanoillis <71679915+miyanoillis@users.noreply.github.com>
2021-01-05 16:02:26 +0800

    1.8

--- Unity_107051053_3DG/Assets/script/enmy.cs
+++ Unity_107051053_3DG/Assets/script/enmy.cs
@@ -9,17 +9,30 @@ public class enmy : MonoBehaviour
     public Animator ani;
     public float cd = 2f;
     private float timer;
+    public player pal;
+    public Transform trak;
 
     [Header("speed") ,Range(0f,50f)]
     public float spd = 3;
     [Header("distance"), Range(0f, 50f)]
     public float spdstop = 1;
 
+    [Header("distance"), Range(0f, 5f)]
+    public float atkln;
+    public Transform atkpt;
+    private RaycastHit rhit;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(atkpt.position, atkpt.forward * atkln);
+    }
 
     private void Awake()
     {
         nav = GetComponent<NavMeshAgent>();
         ani = GetComponent<Animator>();
+        trak = GameObject.Find("01").transform;
 
         player = GameObject.Find("free_male_1").transform;
         nav.speed = spd;
@@ -28,8 +41,17 @@ public class enmy : MonoBehaviour
 
     public void Update()
     {
-        track();
-        attack();
+            track();
+        if(pal.hp >0)
+        {
+            attack();
+        }
+        else
+        {
+            Vector3 pos = trak.position;
+            pos.y = transform.position.y;
+            transform.LookAt(pos);
+        }
     }
 
     public void attack()
@@ -39,20 +61,34 @@ public class enmy : MonoBehaviour
             timer += Time.deltaTime;
 
             Vector3 pos = player.position;
-            pos.y = player.position.y;
+            pos.y = transform.position.y;
             transform.LookAt(pos);
 
             if (timer >= cd)
             {
             ani.SetTrigger("atk");
                 timer = 0;
+
+                if(Physics.Raycast(atkpt.position, atkpt.forward, out rhit, atkln, 1 << 8))
+                {
+                    rhit.collider.GetComponent<player>().dmg();
+                }
             }
         }
     }
 
     public void track()
     {
+        if(pal.hp >= 0)
+        {
         nav.SetDestination(player.position);
         ani.SetBool("walk" ,nav.remainingDistance> spdstop);
+        }
+        else
+        {
+            nav.SetDestination(trak.position);
+            ani.SetBool("walk", nav.remainingDistance > spdstop);
+        }
+
     }
 }
---
+++ Unity_107051053_3DG/Assets/script/player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Invector.vCharacterController;
+
+public class player : MonoBehaviour
+{
+    public int hp = 10;
+    public Animator pani;
+
+    private void Awake()
+    {
+        pani = GetComponent<Animator>();
+    }
+
+    public void dmg()
+    {
+        hp -= 5;
+        pani.SetTrigger("hrt");
+
+        if(hp <= 0)
+        {
+            die();
+        }
+
+    }
+    public void die()
+    {
+        pani.SetTrigger("die");
+        vThirdPersonController vt = GetComponent<vThirdPersonController>();
+        vt.lockMovement = true;
+            vt.lockRotation = true;
+
+    }
+}
